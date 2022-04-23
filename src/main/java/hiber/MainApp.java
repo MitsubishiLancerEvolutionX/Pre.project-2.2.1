f3b27d2c2000370e2c2007ae00f58457dbd39eef
MitsubishiLancerEvolutionX <92749312+MitsubishiLancerEvolutionX@users.noreply.github.com>
2022-04-24 02:04:08 +0300

    Pre.project-2.2.1
    Pre.project-2.2.1

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,53 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.persistence.NoResultException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+      Car car1 = new Car("MitsubishiLancer", 10);
+      Car car2 = new Car("InfinitiQX", 70);
+
+      userService.add(user1.setCar(car1).setUser(user1));
+      userService.add(user2.setCar(car2).setUser(user2));
+      userService.add(user3);
+      userService.add(user4);
+
+      List<User> users = userService.listUsers();
+
+      for (User user : users) {
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
+         System.out.println("Car = " + user.getCar());
+         System.out.println();
+      }
+
+      try {
+         System.out.println(userService.getUserWithCar("InfinitiQX", 70));
+         System.out.println(userService.getUserWithCar("MitsubishiLancer", 9));
+         //System.out.println(userService.getUserByCar("MitsubishiLancer", 10));
+      } catch (NoResultException e) {
+         System.out.println("Нет пользователя, владеющего такой машиной!");
+      }
+
+      context.close();
+   }
+}
